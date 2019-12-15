4b16f2beed2f5cef3441a875a0e5311c1d59810a
Flemming Jahn <flemming@familien-jahn.dk>
2019-12-15 19:56:01 +0100

    Still working

--- c.py
+++ c.py
@@ -7,40 +7,44 @@ import shutil
 
 class Cheatsheet:
     sections = []  # Array of section containning the chaeats
+    cheatSheetsPath = os.path.dirname(os.path.realpath(__file__)) + "/my_cheatsheets"  # Get the path to the cheatsheets
+    currentCheatSheetFullPath = None
 
     def readYamlFile(self, fileName):
         if os.path.exists(fileName) and os.path.getsize(fileName) > 0:
             self.sections = yaml.load(fileName)
 
-    def getCheatSheetsPath(self):
-        return os.path.dirname(os.path.realpath(__file__)) + "/my_cheatsheets"  # Get the path to the cheatsheets
+    def currentCheatSheetNameFullPathGet(self):
+        return f"{self.cheatSheetsPath}/{self.currentCheatSheatName}.yaml"
 
     # Function for find a file that matches the file given by the used.
     # If multiple files matches, then print the list of files, and exit
-    def findFileName(self, cheatName):
-        if cheatName != None:
-            if path.exists(cheatName):
-                return f"{cheatName}.yaml"
+    def findFileName(self):
+        self.currentCheatSheatName = self.cheatSheetNameGet()
+
+        if self.currentCheatSheatName != None:
+            if os.path.exists(self.currentCheatSheetNameFullPathGet()):
+                return f"{self.currentCheatSheatName}.yaml"
 
 
         pwd = os.getcwd()
-        os.chdir(self.getCheatSheetsPath())
-        if cheatName == None:
+        os.chdir(self.cheatSheetsPath)
+        if self.currentCheatSheatName == None:
             files = glob.glob('*.yaml') # Get all cheatsheets
         else:
-            files = glob.glob(f"{cheatName}*.yaml")
+            files = glob.glob(f"{self.currentCheatSheatName}*.yaml")
         os.chdir(pwd)
 
-        if not files and cheatName != None:
+        if not files and self.currentCheatSheatName != None:
             # Create a new cheatSheet
             print("New cheatSheet named #{cheatName} created")
-            return f"#{cheatName}.yaml"
+            return f"#{currentCheatSheatName}.yaml"
 
         if not files:
             print ("No cheatsheet files found")
             sys.exit()
 
-        if cheatName == None and len(files) == 1:
+        if self.currentCheatSheatName == None and len(files) == 1:
             f = files[0].gsub("\.yaml", "")
             print(f"One cheatsheet file found : {f}")
             sys.exit()
@@ -65,6 +69,96 @@ class Cheatsheet:
         # Only one file found.
         return f"{files[0]}"
 
+    currentCheatSheatName = ""
+    scriptPath = os.path.dirname(sys.argv[0])
+    lastCheatSheet = f"{scriptPath}/cheatsheet.last"
+
+
+    # If the first argument is a number then we copy the corresponding copyText from the last sheet that was read
+    def cheatSheetNameGet(self):
+        if unknownargs:
+            # user provided cheatsheet
+            self.storeLastCheatName()
+            return unknownargs[0]
+
+        # If the user didn't provide a cheat sheet name, will we list all possible cheatsheet
+        if len(sys.argv) == 1:
+            return None
+
+        if not os.path.exists(self.lastCheatSheet):
+            print("No cheatsheeet has been read yet - Can't do copytext until a sheet has been read.\n")
+            sys.exit()
+
+        with open(self.lastCheatSheet) as infile:
+            return yaml.load(infile)
+
+
+    def storeLastCheatName(self):
+        with open(self.lastCheatSheet, "w") as file:
+            yaml.dump(self.currentCheatSheatName, file)
+
+    def readCheatSheet(self):
+        with open(self.currentCheatSheetNameFullPathGet()) as infile:
+            return yaml.safe_load(infile)
+
+    def sectionLengthMax(self):
+        maxLen = 0
+        cheats = self.readCheatSheet()
+        for cheat in cheats:
+            for section in cheat.keys():
+                if section.__len__() > maxLen:
+                    maxLen = section.__len__()
+        return maxLen
+
+    def lengthsMax(self):
+        sectionMaxLen = 0
+        descrMaxLength = 0
+        copyTxtMaxLength = 0
+        for sections in self.readCheatSheet():
+            for section in sections.keys():
+                for cheat in sections[section]:
+                    if section.__len__() > sectionMaxLen:
+                        sectionMaxLen = section.__len__()
+
+                    if cheat['Descr'] != None and cheat['Descr'].__len__() > descrMaxLength:
+                        descrMaxLength = cheat['Descr'].__len__()
+
+                    if cheat['CopyTxt'] != None and cheat['CopyTxt'].__len__() > copyTxtMaxLength:
+                        copyTxtMaxLength = cheat['CopyTxt'].__len__()
+
+        return sectionMaxLen, descrMaxLength, copyTxtMaxLength
+
+    def printHeader(self, section, descrMaxLength, copyTxtMaxLength):
+        totalLength = descrMaxLength + copyTxtMaxLength + 15 # Add 15 for spaces and section number and etc.
+        sectionLength = section.__len__()
+
+        if totalLength % 2:
+            totalLength += 1
+
+        if sectionLength % 2:
+            section += " "
+            sectionLength += 1
+
+        dashLength = round((totalLength - sectionLength) / 2)
+
+        text = "=" * dashLength + f" {section} " + "=" * dashLength
+        print(text)
+
+    def printCheatsheet(self):
+        allCheats = self.readCheatSheet()
+        sectionNumber = 1
+        sectionMaxLen, descrMaxLength, copyTxtMaxLength = self.lengthsMax()
+
+        for sections in allCheats:
+            for section in sections.keys():
+                self.printHeader(section, descrMaxLength, copyTxtMaxLength )
+                for cheat in sections[section]:
+                    descr   = str(cheat['Descr'] or '')
+                    copyTxt = str(cheat['CopyTxt'] or '')
+
+                    print(f"{sectionNumber:<{2}} - {descr:{descrMaxLength}} : {copyTxt}")
+                    sectionNumber += 1
+            print()
 
 helpTxt = "If you like me are working with a Linux terminal all day long,\n" \
           "but also have as poor a memory as me,\n"\
@@ -95,23 +189,8 @@ helpTxt = "If you like me are working with a Linux terminal all day long,\n" \
           "Result : 'git clean -df; git reset --hard' copied to clipboard, now you can paste the 'git clean -df; git reset --hard' command"
 
 
-def cheatSheetNameGet():
-    if unknownargs:
-        # user provided cheatsheet
-        cheatSheetName = unknownargs[0]
 
-        if cheatSheetName.isdecimal():
-            scriptPath = os.path.dirname(sys.argv[0])
 
-        lastCheatSheet = f"{scriptPath}/cheatsheet.last"
-        if not os.path.exists(lastCheatSheet):
-            print("No cheatsheeet has been read yet - Can't do copytext until a sheet has been read.\n")
-            sys.exit()
-
-        with open(lastCheatSheet) as infile:
-            return yaml.load(infile)
-
-    return None
 
 import argparse
 parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.RawTextHelpFormatter)
@@ -121,16 +200,14 @@ parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, he
 args, unknownargs = parser.parse_known_args()
 
 
-cheatSheetName = cheatSheetNameGet()
-
-
 
 c = Cheatsheet()
 
-file = c.findFileName(cheatSheetName)
+file = c.findFileName()
+c.printCheatsheet()
 
-with open(f"#{c.getCheatSheetsPath()}/cheatsheet.last", 'w') as outfile:
-    yaml.dump(cheatSheetName, outfile)
+
+
 
 
 
